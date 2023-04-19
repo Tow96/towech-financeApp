35f30d907e5711052b0f294e545286685e356263
Jose Tow <jose.towe@gmail.com>
2023-04-18 18:47:38 -0600

    Created build CI

--- libs/desktop/shell/data-access/authentication/src/lib/desktop-shell-data-authentication.service.ts
+++ libs/desktop/shell/data-access/authentication/src/lib/desktop-shell-data-authentication.service.ts
@@ -12,9 +12,7 @@ export class DesktopAuthenticationService {
   login(credentials: LoginCredentials) {
     return timer(2000).pipe(
       switchMap(() =>
-        credentials.username !== 'fail'
-          ? { message: 'success' }
-          : throwError(() => new Error('Fail'))
+        credentials.username !== 'fail' ? 'success' : throwError(() => new Error('Fail'))
       )
     );
   }
