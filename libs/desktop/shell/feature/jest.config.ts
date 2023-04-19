fc8414f4597d767382a11d4325888802b7ef0ea3
Jose Tow <jose.towe@gmail.com>
2023-04-18 19:29:11 -0600

    Testing coverage report

--- libs/desktop/shell/feature/jest.config.ts
+++ libs/desktop/shell/feature/jest.config.ts
@@ -4,6 +4,7 @@ export default {
   preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../../../coverage/libs/desktop/shell/feature',
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
