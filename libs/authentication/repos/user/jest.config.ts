23d1f35d41abceadddcbe4669877c91f5d9b5d94
Jose Tow <jose.towe@gmail.com>
2023-04-19 17:07:33 -0600

    Added Testing

--- libs/authentication/repos/user/jest.config.ts
+++ libs/authentication/repos/user/jest.config.ts
@@ -8,4 +8,6 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/libs/authentication/repos/user',
+  collectCoverageFrom: ['**/*.(t|j)s', '!**/*.module.ts', '!**/index.ts', '!**/jest.config.ts'],
+  coverageReporters: ['html', 'text', 'text-summary', 'cobertura', 'lcov'],
 };
