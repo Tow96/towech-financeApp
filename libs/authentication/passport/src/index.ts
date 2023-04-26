c953eb913869d8c7aa78dcc5e254b7adb266be7d
Jose Tow <jose.towe@gmail.com>
2023-04-26 15:40:56 -0600

    Added user decorator

--- libs/authentication/passport/src/index.ts
+++ libs/authentication/passport/src/index.ts
@@ -1,4 +1,5 @@
 export { AuthenticationPassportModule } from './lib/authentication-passport.module';
-export { LocalAuthGuard } from './lib/local-auth.guard';
-export { JwtRefreshGuard } from './lib/jwt-refresh.guard';
 export { JwtAuthGuard } from './lib/jwt-auth.guard';
+export { JwtRefreshGuard } from './lib/jwt-refresh.guard';
+export { LocalAuthGuard } from './lib/local-auth.guard';
+export { User } from './lib/user.decorator';
