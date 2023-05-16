b0d19b53689d463a9c066aefba1d132416d82a78
Jose Tow <jose.towe@gmail.com>
2023-05-16 12:28:19 -0600

    Added constructor

--- libs/shared/utils/models/src/lib/refreshToken.model.ts
+++ libs/shared/utils/models/src/lib/refreshToken.model.ts
@@ -3,4 +3,9 @@ import { UserModel } from './user.model';
 export class RefreshToken {
   id: string;
   user: UserModel;
+
+  constructor(id: string, user: UserModel) {
+    this.id = id;
+    this.user = user;
+  }
 }
