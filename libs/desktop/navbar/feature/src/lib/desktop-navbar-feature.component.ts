e0300771f9fdedf96d6f0e1a28fd662e150a23a4
Jose Tow <jose.towe@gmail.com>
2023-05-18 14:51:36 -0600

    Added NavBar Component

---
+++ libs/desktop/navbar/feature/src/lib/desktop-navbar-feature.component.ts
@@ -0,0 +1,17 @@
+/** desktop-navbar-feature.component.ts
+ * Copyright (c) 2023, Towechlabs
+ *
+ * Navbar for the complete app
+ */
+
+import { Component } from '@angular/core';
+
+// Libraries
+@Component({
+  standalone: true,
+  selector: 'towech-finance-webclient-navbar',
+  imports: [],
+  styleUrls: ['./desktop-navbar-feature.component.scss'],
+  template: `<p>DesktopNavbarComponent works</p>`,
+})
+export class DesktopNavbarComponent {}
