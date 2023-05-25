32975665affcf8336c28908ca7c5b3f2e776916f
Jose Tow <jose.towe@gmail.com>
2023-05-25 14:21:30 -0600

    Added colors to the button component

--- libs/shared/ui/button/src/lib/shared-ui-button.component.ts
+++ libs/shared/ui/button/src/lib/shared-ui-button.component.ts
@@ -10,8 +10,9 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   selector: 'towech-finance-button',
   styleUrls: ['shared-ui-button.component.scss'],
-  template: `<button [type]="type"><ng-content></ng-content></button>`,
+  template: `<button [className]="color" [type]="type"><ng-content></ng-content></button>`,
 })
 export class ButtonComponent {
   @Input() public type: 'button' | 'menu' | 'reset' | 'submit' = 'button';
+  @Input() public color: 'accent' | 'success' | 'error' | 'warning' = 'accent';
 }
