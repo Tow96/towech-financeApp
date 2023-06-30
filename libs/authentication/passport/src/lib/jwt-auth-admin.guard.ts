fd26acedef87bfa4d6098dd404ebdaeb6e65c442
Jose Tow <jose.towe@gmail.com>
2023-06-30 15:22:17 -0600

    Working on guard i18n

--- libs/authentication/passport/src/lib/jwt-auth-admin.guard.ts
+++ libs/authentication/passport/src/lib/jwt-auth-admin.guard.ts
@@ -5,9 +5,11 @@
  */
 
 // Libraries
-import { Injectable, HttpException } from '@nestjs/common';
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard, PassportStrategy } from '@nestjs/passport';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { I18nContext } from 'nestjs-i18n';
 // Services
 import { ConfigService } from '@nestjs/config';
 import { AuthenticationUserService } from '@towech-finance/authentication/repos/user';
@@ -30,13 +32,35 @@ export class JwtAuthAdminStrategy extends PassportStrategy(Strategy, StrategyNam
   }
 
   public async validate(payload: UserModel): Promise<UserModel> {
+    const i18n = I18nContext.current();
     const user = await this.userRepo.getById(payload._id);
+
     if (!user || user.role !== UserRoles.ADMIN) {
-      throw new HttpException('Invalid credentials', 401);
+      throw new UnauthorizedException(i18n.t('validation.INVALID_CREDENTIALS'));
     }
 
     return user;
   }
 }
 
-export class JwtAuthAdminGuard extends AuthGuard(StrategyNames.AUTH_ADMIN) {}
+export class JwtAuthAdminGuard extends AuthGuard(StrategyNames.AUTH_ADMIN) {
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  public handleRequest<TUser = UserModel>(
+    err: any,
+    user: UserModel | false,
+    info: any,
+    context: ExecutionContext,
+    status?: any
+  ): TUser {
+    // console.log(info instanceof AuthenticationError);
+    console.log(info.status);
+    // console.log(JSON.stringify(info));
+    const i18n = I18nContext.current();
+
+    if (info instanceof JsonWebTokenError || info?.message === 'No auth token') {
+      throw new UnauthorizedException(i18n.t('validation.INVALID_CREDENTIALS'));
+    }
+    return super.handleRequest(err, user, info, context, status);
+  }
+  /* eslint-enable @typescript-eslint/no-explicit-any */
+}
