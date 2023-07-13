7cbccf40b6431c0aa4db7fc3a63424cbde3e87bd
Jose Tow <jose.towe@gmail.com>
2023-07-13 07:57:12 -0600

    Moved styles to component

--- libs/desktop/shell/ui/layout/src/lib/layout.component.ts
+++ libs/desktop/shell/ui/layout/src/lib/layout.component.ts
@@ -12,17 +12,31 @@ import { DesktopToasterComponent } from '@towech-finance/desktop/toasts/tray';
   selector: 'towech-finance-webclient-layout',
   imports: [DesktopToasterComponent],
   // imports: [DesktopNavbarComponent, DesktopToasterComponent, RouterModule],
-  styleUrls: ['layout.component.scss'],
+  styles: [
+    `
+      .layout {
+        display: flex;
+
+        .contents {
+          flex: 1;
+          margin: 1em;
+        }
+
+        @media screen and (max-width: 30em) {
+          flex-direction: column;
+        }
+      }
+    `,
+  ],
   template: `
     <towech-finance-toaster></towech-finance-toaster>
-    <button (click)="testhrow()">throw</button>
-    <!-- <towech-finance-toaster></towech-finance-toaster>
     <div class="layout">
-      <towech-finance-webclient-navbar></towech-finance-webclient-navbar>
+      <button (click)="testhrow()">throw</button>
+      <!-- <towech-finance-webclient-navbar></towech-finance-webclient-navbar> -->
       <div class="contents">
-        <router-outlet></router-outlet>
+        <!-- <router-outlet></router-outlet> -->
       </div>
-    </div> -->
+    </div>
   `,
 })
 export class LayoutComponent {
