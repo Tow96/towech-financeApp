5c09a4593139fae46d68d051b2cfcde3ac9ba78c
Jose Tow <jose.towe@gmail.com>
2023-07-25 17:22:52 -0600

    Added tests for environment

--- libs/desktop/environment/src/lib/environment.spec.ts
+++ libs/desktop/environment/src/lib/environment.spec.ts
@@ -1,13 +1,26 @@
-import { environment as local } from './environment';
+// Libraries
+import { TestBed } from '@angular/core/testing';
+// Tested elements
+import { APP_CONFIG, provideEnvironment } from './environment.service';
+import { environment as base } from './environment';
 import { environment as prd } from './environment.prod';
 
 describe('environments', () => {
   let properties: string[];
   beforeAll(() => {
-    properties = Object.keys(local).sort();
+    properties = Object.keys(base).sort();
   });
 
   test('PRD must have the same properties as the base', () => {
     expect(Object.keys(prd).sort()).toEqual(properties);
+  });
+});
+
+describe('environment service', () => {
+  it('Should get a provider containing the service', () => {
+    expect(provideEnvironment()).toEqual({
+      provide: APP_CONFIG,
+      useValue: base,
+    });
   });
 });
