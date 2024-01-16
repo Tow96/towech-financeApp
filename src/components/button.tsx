43f790a4336069056f6d61834c4a349286656337
Jose Tow <jose.towe@gmail.com>
2024-01-16 15:53:48 -0600

    Tweaked sizes and props

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -27,7 +27,7 @@ export const Button = ({
       {...props}
       disabled={disabled}
       type={type}
-      className={`button-shadow active:input-shadow rounded-lg px-4 py-2 text-lg font-semibold transition-shadow focus-visible:outline-none ${variableClasses}`}
+      className={`button-shadow active:input-shadow rounded-lg px-3 py-2 font-semibold transition-shadow focus-visible:outline-none ${variableClasses}`}
     />
   );
 };
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -5,14 +5,21 @@
  */
 import { UseFormRegisterReturn } from 'react-hook-form';
 
-type Props = {
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
   label: string;
   register?: UseFormRegisterReturn<string>;
   type?: 'text' | 'password';
-  error?: boolean;
-};
+}
 
-export const Input = ({ register, label, error, type = 'text' }: Props): JSX.Element => {
+export const Input = ({
+  register,
+  label,
+  error,
+  type = 'text',
+  className,
+  ...props
+}: Props): JSX.Element => {
   const id = label.replace(/\s/g, '').toLowerCase();
   const inputVariableClasses = error
     ? `focus-visible:focused-input-error`
@@ -22,18 +29,19 @@ export const Input = ({ register, label, error, type = 'text' }: Props): JSX.Ele
     : `text-riverbed-50 font-normal`;
 
   return (
-    <div className="relative mx-1 mb-5 mt-7">
+    <div className="relative mx-1 mb-5 mt-7 w-full">
       <input
         type={type}
         placeholder=""
         {...register}
         id={id}
-        className={`${inputVariableClasses} input-shadow peer box-border w-full rounded border-none bg-riverbed-800 px-4 py-2 text-2xl font-normal text-riverbed-50 focus:outline-none`}
+        className={`${inputVariableClasses} input-shadow peer box-border w-full rounded border-none bg-riverbed-800 px-2 py-1 text-lg font-normal text-riverbed-50 focus:outline-none ${className}`}
+        {...props}
       />
       <label
         data-testid="label"
         htmlFor={id}
-        className={`${labelVariableClasses} pointer-events-none absolute left-0 top-0 mx-1 my-1 origin-top-left translate-x-0 translate-y-0 whitespace-nowrap bg-transparent px-1 py-1 text-2xl transition-all peer-focus:-translate-y-2/3 peer-focus:translate-x-1 peer-focus:scale-[0.57] peer-[:not(:placeholder-shown)]:-translate-y-2/3 peer-[:not(:placeholder-shown)]:translate-x-1 peer-[:not(:placeholder-shown)]:scale-[0.57] `}>
+        className={`${labelVariableClasses} pointer-events-none absolute left-0 top-0 mx-1 my-1 origin-top-left -translate-y-[10%] translate-x-0 whitespace-nowrap bg-transparent px-1 py-1 text-lg transition-all peer-focus:-translate-y-2/3 peer-focus:translate-x-1 peer-focus:scale-[0.57] peer-[:not(:placeholder-shown)]:-translate-y-2/3 peer-[:not(:placeholder-shown)]:translate-x-1 peer-[:not(:placeholder-shown)]:scale-[0.57] `}>
         {label}
       </label>
     </div>
