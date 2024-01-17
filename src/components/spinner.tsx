7dba688e29e81dc18ef49bea2283d6d0d1a08670
Jose Tow <jose.towe@gmail.com>
2024-01-17 10:36:31 -0600

    Added colors and sizes to the spinner

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -1,16 +1,27 @@
+import { classNames } from '@/utils/ConditionalClasses';
+
 /** spinner.tsx
  * Copyright (c) 2023, Towechlabs
  *
  * Simple loading spinner
  */
-// type Props = {
-//   size?: number;
-// };
-// export const Spinner = ({ size = 60 }: Props): JSX.Element => {
-export const Spinner = (): JSX.Element => {
+type Props = {
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  color?: '300' | '600' | '950';
+  className?: string;
+};
+export const Spinner = ({ size = 'lg', color = '300', className }: Props): JSX.Element => {
+  const variableClasses = classNames({
+    'w-24 border-8': size === 'lg',
+    'w-4 border-2': size === 'xs',
+
+    'border-riverbed-300': color === '300',
+    'border-riverbed-600': color === '600',
+    'border-riverbed-950': color === '950',
+  });
   return (
     <span
       role="status"
-      className={`aspect-square w-24 animate-spin rounded-full border-8 border-solid border-riverbed-300 border-b-transparent border-t-transparent`}></span>
+      className={`block aspect-square animate-spin rounded-full border-solid border-b-transparent border-t-transparent ${variableClasses} ${className}`}></span>
   );
 };
