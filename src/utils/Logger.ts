849a4af1d6dfb8e44650d740e27ad5a94708936e
Jose Tow <jose.towe@gmail.com>
2024-02-19 11:06:32 -0600

    Added pino logging

---
+++ src/utils/Logger.ts
@@ -0,0 +1,10 @@
+/** Logger.ts
+ * Copyright (c) 2024, Towechlabs
+ *
+ * adapter for pino
+ */
+import pino, { Logger } from 'pino';
+
+const level = process.env.NODE_ENV === 'development' ? 'debug' : 'info';
+export const getLogger = (name: string): Logger =>
+  pino({ name, level, transport: { target: 'pino-pretty', options: { colorize: true } } });
