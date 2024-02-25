31ee01b0bfbe2c50bf84f77c997869905bd41aa6
Jose Tow <jose.towe@gmail.com>
2024-02-25 15:09:27 -0600

    Added setup for server tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -8,13 +8,14 @@ const clientTestConfig = await createJestConfig({
   displayName: 'client',
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/__tests__/client/**/*.[jt]s?(x)'],
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest/jest.client.setup.ts']
 })();
 
 const serverTestConfig = await createJestConfig({
   displayName: 'server',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/server/**/*.[jt]s?(x)'],
+  setupFiles: ['<rootDir>/jest/jest.server.setup.ts']
 })();
 
 
