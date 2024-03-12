6ea666ab242ef87fa882ea122f159196680aeb23
Jose Tow <jose.towe@gmail.com>
2024-03-11 20:10:18 -0600

    temporarily disabled pino-pretty

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -6,5 +6,5 @@
 import pino, { Logger } from 'pino';
 
 const level = process.env.NODE_ENV === 'development' ? 'debug' : 'info';
-export const getLogger = (name: string): Logger =>
-  pino({ name, level, transport: { target: 'pino-pretty', options: { colorize: true } } });
+export const getLogger = (name: string): Logger => pino({ name, level });
+// pino({ name, level, transport: { target: 'pino-pretty', options: { colorize: true } } });
