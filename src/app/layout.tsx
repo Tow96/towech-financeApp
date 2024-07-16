4c2b008fdb5952e1eb98598c484d45db6b11159a
Jose Tow <jose.towe@gmail.com>
2024-07-16 13:22:50 -0600

    Optimized font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Raleway } from 'next/font/google';
 import 'normalize.css';
 import '../index.css';
 
@@ -7,9 +8,14 @@ export const metadata: Metadata = {
   description: 'Web site created with Next.js.',
 };
 
+const raleway = Raleway({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={raleway.className}>
       <body>
         <div id="root">{children}</div>
       </body>
