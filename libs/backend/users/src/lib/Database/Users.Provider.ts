7f00f263e14f50f41af7df31a44a8dc30ff10545
Jose Tow <jose.towe@gmail.com>
2025-01-20 10:17:18 -0600

    Cleaned code

--- libs/backend/users/src/lib/Database/Users.Provider.ts
+++ libs/backend/users/src/lib/Database/Users.Provider.ts
@@ -1,17 +1,31 @@
-import { Pool } from 'pg';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { UsersSchema } from './Users.Schema';
 import { ConfigService } from '@nestjs/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+import { Pool } from 'pg';
+import { UsersSchema } from './Users.Schema';
+
+// Repositories
+import { EmailVerificationRepository } from './Repositories/EmailVerification.Repository';
+import { PasswordResetRepository } from './Repositories/PasswordReset.Repository';
+import { UserInfoRepository } from './Repositories/UserInfo.Repository';
 
 export const USER_SCHEMA_CONNECTION = 'DATABASE_CONNECTION';
 
-export const UsersProvider = {
-  provide: USER_SCHEMA_CONNECTION,
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService) => {
-    const connectionString = configService.getOrThrow<string>('DATABASE_URL');
-    const pool = new Pool({ connectionString });
+export const UserProviders = [
+  // DB
+  {
+    provide: USER_SCHEMA_CONNECTION,
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => {
+      const connectionString = configService.getOrThrow<string>('DATABASE_URL');
+      const pool = new Pool({ connectionString });
 
-    return drizzle(pool, { schema: UsersSchema });
+      return drizzle(pool, { schema: UsersSchema });
+    },
   },
-};
+
+  // Repos
+  EmailVerificationRepository,
+  PasswordResetRepository,
+  UserInfoRepository,
+];
--- libs/backend/users/src/lib/Users.Module.ts
+++ libs/backend/users/src/lib/Users.Module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { UsersProvider, USER_SCHEMA_CONNECTION } from './Database/Users.Provider';
+import { UserProviders, USER_SCHEMA_CONNECTION } from './Database/Users.Provider';
 
 // Controllers
 import { UserController } from './Api/Controllers/User.Controller';
@@ -7,19 +7,9 @@ import { EmailController } from './Api/Controllers/Email.Controller';
 import { PasswordController } from './Api/Controllers/Password.Controller';
 import { SessionController } from './Api/Controllers/Session.Controller';
 
-// Repositories
-import { UserInfoRepository } from './Database/Repositories/UserInfo.Repository';
-import { EmailVerificationRepository } from './Database/Repositories/EmailVerification.Repository';
-import { PasswordResetRepository } from './Database/Repositories/PasswordReset.Repository';
-
 @Module({
   controllers: [UserController, EmailController, PasswordController, SessionController],
-  providers: [
-    UsersProvider,
-    UserInfoRepository,
-    EmailVerificationRepository,
-    PasswordResetRepository,
-  ],
+  providers: [...UserProviders],
   exports: [USER_SCHEMA_CONNECTION],
 })
 export class UsersModule {}
