94e031f637ddf30a3e092d7eb4e4afaf0789b9a0
Jose Tow <jose.towe@gmail.com>
2025-06-12 20:10:34 -0600

    Created users-backend package

---
+++ packages/users/backend/README.md
@@ -0,0 +1,7 @@
+# users-backend
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build users-backend` to build the library.
---
+++ packages/users/backend/src/lib/user.entity.ts
@@ -0,0 +1,12 @@
+import { User as ClerkUser } from '@clerk/backend';
+
+export class User {
+  private clerkUser: ClerkUser;
+  constructor(clerkUser: ClerkUser) {
+    this.clerkUser = clerkUser;
+  }
+
+  get id() {
+    return this.clerkUser.id;
+  }
+}
