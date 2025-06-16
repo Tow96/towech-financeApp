602d09aa218b1f78a0edebec836f4199d1246d97
Jose Tow <jose.towe@gmail.com>
2025-06-16 08:09:52 -0600

    Added system theme to app

--- webclient/src/app/layout.tsx
+++ webclient/src/app/layout.tsx
@@ -11,6 +11,7 @@ import { Separator } from '@/lib/shadcn-ui/components/ui/separator';
 import { WebClientSidebar } from '@/lib/webclient/sidebar';
 import { UsersProvider } from '@/lib/users';
 import { QueryProvider } from '@/lib/webclient/QueryProvider';
+import { ThemeProvider } from '@/lib/webclient/theme-provider';
 
 export const metadata: Metadata = {
   title: 'Towechlabs Finance App',
@@ -26,21 +27,27 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
       <html lang="en">
         <body>
           <QueryProvider>
-            <SidebarProvider defaultOpen={openSidebar}>
-              <WebClientSidebar />
-              <SidebarInset>
-                {/* Header */}
-                <header className="flex h-16 shrink-0 items-center gap-2 border-b">
-                  <div className="flex items-center gap-2 px-3">
-                    <SidebarTrigger />
-                    <Separator orientation="vertical" className="mr-2 h-4" />
-                    TODO: TITLE
-                  </div>
-                </header>
-                {/* Content */}
-                <main>{children}</main>
-              </SidebarInset>
-            </SidebarProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange>
+              <SidebarProvider defaultOpen={openSidebar}>
+                <WebClientSidebar />
+                <SidebarInset>
+                  {/* Header */}
+                  <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+                    <div className="flex items-center gap-2 px-3">
+                      <SidebarTrigger />
+                      <Separator orientation="vertical" className="mr-2 h-4" />
+                      TODO: TITLE
+                    </div>
+                  </header>
+                  {/* Content */}
+                  <main>{children}</main>
+                </SidebarInset>
+              </SidebarProvider>
+            </ThemeProvider>
           </QueryProvider>
         </body>
       </html>
---
+++ webclient/src/lib/webclient/theme-provider.tsx
@@ -0,0 +1,7 @@
+﻿'use client';
+import { ComponentProps, ReactNode } from 'react';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+
+export function ThemeProvider({ children, ...props }: ComponentProps<typeof NextThemesProvider>): ReactNode {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
