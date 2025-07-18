0d484bfb561320d887960eb7bb93e6c5df354d74
Jose Tow <jose.towe@gmail.com>
2025-07-18 13:53:34 -0600

    Added tanstack mutations

---
+++ apps/webclient/src/lib/wallets/data-store/use-archive-wallet.ts
@@ -0,0 +1,26 @@
+﻿import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useUsers } from '@/lib/users/use-users';
+
+import { WALLET_QUERY_KEY } from './use-wallets';
+import ApiClient from '@/lib/api';
+
+interface ArchiveWalletDto {
+  id: string;
+  restore: boolean;
+}
+
+export const useArchiveWallet = () => {
+  const api = new ApiClient(useUsers().getToken());
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data: ArchiveWalletDto) =>
+      api.put<undefined>(
+        `category/${data.id}/archive?restore=${data.restore ? 'true' : 'false'}`,
+        data
+      ),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: [WALLET_QUERY_KEY] });
+    },
+  });
+};
