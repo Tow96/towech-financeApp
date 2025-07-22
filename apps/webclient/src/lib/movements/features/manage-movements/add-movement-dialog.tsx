0312077bc183f6477ed76198f343b88a43b46e38
Jose Tow <jose.towe@gmail.com>
2025-07-21 18:46:48 -0600

    Added datepicker and amount fields

--- apps/webclient/src/lib/movements/features/manage-movements/add-movement-dialog.tsx
+++ apps/webclient/src/lib/movements/features/manage-movements/add-movement-dialog.tsx
@@ -1,6 +1,6 @@
 ﻿'use client';
 import { useState, ReactNode } from 'react';
-import { Plus } from 'lucide-react';
+import { CalendarIcon, Plus } from 'lucide-react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -14,6 +14,9 @@ import {
 } from '@/lib/shadcn-ui/components/ui/form';
 import { Button } from '@/lib/shadcn-ui/components/ui/button';
 import { Input } from '@/lib/shadcn-ui/components/ui/input';
+import { Calendar } from '@/lib/shadcn-ui/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/lib/shadcn-ui/components/ui/popover';
+import { cn } from '@/lib/shadcn-ui/utils';
 
 import { FormDialog } from '@/lib/webclient';
 import { useAddMovement } from '@/lib/movements/data-store';
@@ -24,8 +27,9 @@ export const AddMovementDialog = (): ReactNode => {
   const addMovementMutation = useAddMovement();
 
   const formSchema = z.object({
+    amount: z.string().regex(/^\d+(\.\d{1,2})?$/, 'Must be a number with up to two decimal places'),
     category: z.object({
-      id: z.string(),
+      id: z.string().min(1, { message: 'Select a category' }),
       subCategory: z.string().nullable(),
     }),
     date: z.date(),
@@ -37,6 +41,7 @@ export const AddMovementDialog = (): ReactNode => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      amount: '',
       category: {
         id: '',
         subCategory: null,
@@ -47,15 +52,16 @@ export const AddMovementDialog = (): ReactNode => {
   });
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    addMovementMutation.mutate(
-      {
-        categoryId: values.category.id,
-        subCategoryId: values.category.subCategory,
-        date: values.date,
-        description: values.description,
-      },
-      { onSuccess: () => setOpen(false) }
-    );
+    console.log(values);
+    // addMovementMutation.mutate(
+    //   {
+    //     categoryId: values.category.id,
+    //     subCategoryId: values.category.subCategory,
+    //     date: values.date,
+    //     description: values.description,
+    //   },
+    //   { onSuccess: () => setOpen(false) }
+    // );
   };
 
   return (
@@ -72,13 +78,62 @@ export const AddMovementDialog = (): ReactNode => {
         onSubmit={onSubmit}
         error={addMovementMutation.error}
         loading={addMovementMutation.isPending}>
+        {/* Amount */}
+        <FormField
+          control={form.control}
+          disabled={addMovementMutation.isPending}
+          name="amount"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormLabel>Amount</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         {/* Category */}
         <FormField
           control={form.control}
           name="category"
           render={({ field }) => <CategoryFeatures.CategorySelector {...field} />}
         />
-        {/*<CategoryFeatures.CategorySelector />*/}
+
+        {/* Wallet + Date */}
+        <div>
+          {/* Date selector */}
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Date</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant="outline"
+                        className={cn(!field.value && 'text-muted-foreground')}>
+                        {field.value ? field.value.toLocaleDateString() : <span>Pick a date</span>}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      captionLayout="dropdown"
+                    />
+                  </PopoverContent>
+                </Popover>
+              </FormItem>
+            )}
+          />
+        </div>
 
         {/* Description */}
         <FormField
@@ -91,6 +146,7 @@ export const AddMovementDialog = (): ReactNode => {
               <FormControl>
                 <Input {...field} />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
--- apps/webclient/src/lib/movements/features/manage-movements/movement-item/index.tsx
+++ apps/webclient/src/lib/movements/features/manage-movements/movement-item/index.tsx
@@ -5,7 +5,6 @@ import { Skeleton } from '@/lib/shadcn-ui/components/ui/skeleton';
 
 import { Features as CategoryFeatures } from '@/lib/categories';
 import { MovementDto } from '@/lib/movements/data-store';
-import { AppIcon } from '@/lib/icons';
 import { convertNumToCurrencyString } from '@/lib/utils';
 
 interface MovementItemProps {
@@ -18,24 +17,29 @@ const convertIsoDate = (date: string): string => {
 
 export const MovementItem = ({ movement }: MovementItemProps): ReactNode => {
   return (
-    <div className="flex items-center min-w-0 gap-4 py-4 border-b-1 last:border-b-0">
-      <CategoryFeatures.CategoryIcon className="rounded-full w-16 h-16" id={movement.categoryId} />
+    <>
+      <div className="flex items-center min-w-0 gap-4 py-4 border-b-1 last:border-b-0">
+        <CategoryFeatures.CategoryIcon
+          className="rounded-full w-16 h-16"
+          id={movement.categoryId}
+        />
 
-      <div className="flex-1">
-        {/* Main data */}
-        <div className="flex justify-between text-2xl font-bold">
-          <span>
-            <CategoryFeatures.CategoryName id={movement.categoryId} />
-          </span>
-          <span>{convertNumToCurrencyString(0)}</span>
-        </div>
+        <div className="flex-1">
+          {/* Main data */}
+          <div className="flex justify-between text-2xl font-bold">
+            <span>
+              <CategoryFeatures.CategoryName id={movement.categoryId} />
+            </span>
+            <span>{convertNumToCurrencyString(0)}</span>
+          </div>
 
-        {/* Secondary data */}
-        <div className="flex justify-between text-muted-foreground">
-          <span>{movement.description}</span>
-          <span>{convertIsoDate(movement.date)}</span>
+          {/* Secondary data */}
+          <div className="flex justify-between text-muted-foreground">
+            <span>{movement.description}</span>
+            <span>{convertIsoDate(movement.date)}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
