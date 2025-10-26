485001290d522ebd261cab5362f54fda7a4c2a40
Jose Tow <jose.towe@gmail.com>
2025-10-26 11:14:39 -0600

    Added category menu

--- src/features/categories/queries/list-categories/client.tsx
+++ src/features/categories/queries/list-categories/client.tsx
@@ -1,4 +1,5 @@
-﻿import { useQuery } from '@tanstack/react-query'
+﻿import { Ellipsis } from 'lucide-react'
+import { useQuery } from '@tanstack/react-query'
 
 import { categoryKeys } from '../../store-keys'
 import { getCategoriesByType } from './server.ts'
@@ -16,6 +17,14 @@ import { capitalizeFirst, cn } from '@/common/lib/utils'
 
 import { CategoryType } from '@/features/categories/domain'
 import { AddCategoryButton } from '@/features/categories/commands/add-category/client'
+import {
+	DropDrawer,
+	DropDrawerContent,
+	DropDrawerGroup,
+	DropDrawerItem,
+	DropDrawerTrigger,
+} from '@/common/components/ui/dropdrawer'
+import { Button } from '@/common/components/ui/button'
 
 const useCategoryList = (type: CategoryType) => {
 	return useQuery({
@@ -66,15 +75,44 @@ interface CategoryListItemProps {
 const CategoryListItem = ({ category }: CategoryListItemProps) => (
 	<AccordionItem value={category.id}>
 		{/* Main body */}
-		<div>
-			<AccordionTrigger
-				className="flex min-w-0 items-center"
-				empty={category.subCategories.length === 0 || category.archived}>
-				<Icon className="h-12 w-12 rounded-full" id={category.iconId} name={category.name} />
-				<span className="flex-1 overflow-x-hidden text-xl text-nowrap text-ellipsis">
-					{capitalizeFirst(category.name)}
-				</span>
-			</AccordionTrigger>
+		<div className="flex items-center">
+			<div className="flex-1">
+				<AccordionTrigger
+					className="flex min-w-0 items-center"
+					empty={category.subCategories.length === 0 || category.archived}>
+					<Icon className="h-12 w-12 rounded-full" id={category.iconId} name={category.name} />
+					<span className="flex-1 overflow-x-hidden text-xl text-nowrap text-ellipsis">
+						{capitalizeFirst(category.name)}
+					</span>
+				</AccordionTrigger>
+			</div>
+			<DropDrawer>
+				{/* Open menu button */}
+				<DropDrawerTrigger asChild>
+					<Button variant="secondary" size="icon" className="ml-3">
+						<Ellipsis />
+					</Button>
+				</DropDrawerTrigger>
+
+				{/*	Menu content */}
+				<DropDrawerContent align="start">
+					{!category.archived ? (
+						<>
+							<DropDrawerGroup>
+								<DropDrawerItem>Add SubCategory</DropDrawerItem>
+							</DropDrawerGroup>
+							<DropDrawerGroup>
+								<DropDrawerItem>Edit Category</DropDrawerItem>
+								<DropDrawerItem>Archive Category</DropDrawerItem>
+							</DropDrawerGroup>
+						</>
+					) : (
+						<DropDrawerGroup>
+							<DropDrawerItem>Restore Category</DropDrawerItem>
+						</DropDrawerGroup>
+					)}
+				</DropDrawerContent>
+			</DropDrawer>
 		</div>
 
 		{/*	Subcategories */}
