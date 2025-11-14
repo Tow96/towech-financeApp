8c354df719b030f02e79329f21aff18f7ad442a0
Jose Tow <jose.towe@gmail.com>
2025-11-14 13:20:53 -0600

    Dockerized

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: { port: 3000 },
+  server: { port: 3000, host: '0.0.0.0' },
   plugins: [
     tsConfigPaths({
       projects: ['./tsconfig.json'],
