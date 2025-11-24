394cfd5f9d2bd4206fe1271b5c44b420bccc0a33
tow96 <jose.towe@gmail.com>
2025-11-24 17:22:52 -0600

    Added favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -25,8 +25,16 @@ export const Route = createRootRouteWithContext<RouterContext>()({
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ title: 'Towech Finance App' },
 			{ description: 'Keep track of your budgets and expenses' },
+            { name: 'apple-mobile-web-app-title', content: 'FInance App'}
 		],
-		links: [{ rel: 'stylesheet', href: appCss }],
+		links: [
+            { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+            { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+            { rel: 'shortcut icon', href: '/favicon.ico' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+            { rel: 'manifest', href: '/site.webmanifest' },
+            { rel: 'stylesheet', href: appCss }
+        ],
 	}),
 	loader: async () => {
 		const theme = await getThemeServer()
@@ -41,6 +49,7 @@ function RootDocument() {
 	return (
 		<html className={data.theme} lang="en">
 			<head>
+
 				<HeadContent />
 			</head>
 			<body>
