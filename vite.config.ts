4d2ee051286b8ab9acf7fe2d057a5dc682186b1e
tow96 <jose.towe@gmail.com>
2025-11-26 11:59:22 -0600

    Added CORS for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,16 @@ import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	server: { port: 3000, host: '0.0.0.0' },
+	server: {
+		port: 3000,
+		host: '0.0.0.0',
+		cors: {
+			origin: 'https://static.cloudflareinsights.com',
+			credentials: true,
+			methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+			allowedHeaders: ["Content-Type"],
+		}
+	},
 	plugins: [
 		tsConfigPaths({
 			projects: ['./tsconfig.json'],
