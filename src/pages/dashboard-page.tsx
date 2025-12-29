7194e7d4a5f199977bd4dc6163548da76b173194
Jose Tow <jose.towe@gmail.com>
2025-12-29 12:56:29 -0600

    Moved Dashboard page to its own file

---
+++ src/pages/dashboard-page.tsx
@@ -0,0 +1,40 @@
+import { Plus } from 'lucide-react'
+import { useState } from 'react'
+
+import { Button } from '@/common/components/ui/button'
+import { Card, CardContent, CardHeader } from '@/common/components/ui/card'
+
+import { PeriodSelector, PeriodSummary } from '@/features/movements/queries/list-movements/client'
+import { WalletSelector } from '@/features/wallets/queries/list-wallets/client'
+import { AddMovementDialog } from '@/features/movements/commands/add-movement/client'
+import { PeriodMovementList } from '@/features/movements/queries/list-movements/client/movement-list'
+
+export const DashboardPage = () => {
+	const [openAdd, setOpenAdd] = useState<boolean>(false)
+	const [selectedWalletId, setSelectedWalletId] = useState<string | undefined>('total')
+	const [periodStart, setPeriodStart] = useState<Date>(new Date())
+
+	return (
+		<Card className="m-4">
+			<CardHeader className="flex flex-col-reverse items-center justify-between gap-4 md:flex-row">
+				<PeriodSelector start={periodStart} setStart={setPeriodStart} />
+				<WalletSelector
+					className="w-full md:w-1/2"
+					value={selectedWalletId}
+					onChange={setSelectedWalletId}
+					showMoney
+					showTotal
+				/>
+				<Button className="w-full md:w-auto" onClick={() => setOpenAdd(true)}>
+					<Plus /> Add Movement
+				</Button>
+				<AddMovementDialog open={openAdd} setOpen={setOpenAdd} />
+			</CardHeader>
+			<CardContent className="max-h-[62vh] overflow-y-auto">
+				<PeriodSummary selectedWalletId={selectedWalletId} periodStart={periodStart} />
+				<PeriodMovementList selectedWalletId={selectedWalletId} periodStart={periodStart} />
+			</CardContent>
+		</Card>
+	)
+}
+
--- src/routes/_authed/dashboard.tsx
+++ src/routes/_authed/dashboard.tsx
@@ -1,44 +1,7 @@
-import { Plus } from 'lucide-react'
-import { useState } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 
-import { Button } from '@/common/components/ui/button'
-import { Card, CardContent, CardHeader } from '@/common/components/ui/card'
-
-import { AddMovementDialog } from '@/features/movements/commands/add-movement/client'
-import { PeriodSelector, PeriodSummary } from '@/features/movements/queries/list-movements/client'
-import { PeriodMovementList } from '@/features/movements/queries/list-movements/client/movement-list'
-import { WalletSelector } from '@/features/wallets/queries/list-wallets/client'
+import { DashboardPage } from '@/pages/dashboard-page'
 
 export const Route = createFileRoute('/_authed/dashboard')({
-	component: RouteComponent,
+	component: DashboardPage,
 })
-
-function RouteComponent() {
-	const [openAdd, setOpenAdd] = useState<boolean>(false)
-	const [selectedWalletId, setSelectedWalletId] = useState<string | undefined>('total')
-	const [periodStart, setPeriodStart] = useState<Date>(new Date())
-
-	return (
-		<Card className="m-4">
-			<CardHeader className="flex flex-col-reverse md:flex-row items-center justify-between gap-4">
-				<PeriodSelector start={periodStart} setStart={setPeriodStart} />
-				<WalletSelector
-					className="w-full md:w-1/2"
-					value={selectedWalletId}
-					onChange={setSelectedWalletId}
-					showMoney
-					showTotal
-				/>
-				<Button className="w-full md:w-auto" onClick={() => setOpenAdd(true)}>
-					<Plus /> Add Movement
-				</Button>
-				<AddMovementDialog open={openAdd} setOpen={setOpenAdd} />
-			</CardHeader>
-			<CardContent className="max-h-[62vh] overflow-y-auto">
-				<PeriodSummary selectedWalletId={selectedWalletId} periodStart={periodStart} />
-				<PeriodMovementList selectedWalletId={selectedWalletId} periodStart={periodStart} />
-			</CardContent>
-		</Card>
-	)
-}
