cff2a0e3de3cf905fbf2926aa0692e1af0173069
tow96 <jose.towe@gmail.com>
2026-01-15 09:08:17 -0600

    Made movement item logic more responsive

--- src/ui/components/movement-list.tsx
+++ src/ui/components/movement-list.tsx
@@ -18,7 +18,7 @@ import { WalletIcon } from './wallet-icon'
 import type { ListMovementItemDto } from '@/core/contracts'
 
 import { useMovements } from '@/ui/data-access'
-import { capitalizeFirst, cn, convertCentsToCurrencyString } from '@/ui/utils'
+import { capitalizeFirst, cn, convertCentsToCurrencyString, useIsMobile } from '@/ui/utils'
 
 import { CategoryType } from '@/core/entities'
 
@@ -55,81 +55,94 @@ const convertIsoDate = (date: Date): string => {
 }
 
 const MovementItem = ({ movement }: MovementItemProps) => {
+	const isMobile = useIsMobile()
 	const [openEdit, setOpenEdit] = useState<boolean>(false)
 	const [openDelete, setOpenDelete] = useState<boolean>(false)
 
+	// Will open the "edit movement form in mobile"
+	const onItemClick = () => {
+		if (isMobile) setOpenEdit(true)
+	}
+
 	return (
-		<div className="flex min-w-0 items-center gap-4 border-b py-4 last:border-b-0">
+		<>
 			{/* Icon */}
-			<CategoryIcon className="h-10 w-10 md:h-16 md:w-16" category={movement.category} />
+			<div
+				className="flex min-w-0 items-center gap-4 border-b py-4 last:border-b-0"
+				onClick={onItemClick}>
+				{/* Icon */}
+				<CategoryIcon className="h-10 w-10 md:h-16 md:w-16" category={movement.category} />
 
-			{/* Info */}
-			<div className="flex-1">
-				{/* Top row */}
-				<div className="txt-sm flex justify-between font-bold md:text-2xl">
-					<CategoryName category={movement.category} />
-					<span
-						className={cn(
-							movement.category.type === CategoryType.income && 'text-constructive',
-							movement.category.type === CategoryType.expense && 'text-destructive',
-						)}>
-						{convertCentsToCurrencyString(movement.amount)}
-					</span>
-				</div>
-				{/*	Bottom row */}
-				<div className="text-muted-foreground flex justify-between">
-					{/*	Origin and destination*/}
-					<div className="flex">
-						{movement.wallet.originId && (
-							<WalletIcon className="h-5 w-5 md:h-6 md:w-6" walletId={movement.wallet.originId} />
-						)}
-						{movement.wallet.originId && movement.wallet.destinationId && (
-							<ArrowBigRight className="h-5 w-5 md:h-6 md:w-6" />
-						)}
-						{movement.wallet.destinationId && (
-							<WalletIcon
-								className="h-5 w-5 md:h-6 md:w-6"
-								walletId={movement.wallet.destinationId}
-							/>
-						)}
+				{/* Info */}
+				<div className="flex-1">
+					{/* Top row */}
+					<div className="txt-sm flex justify-between font-bold md:text-2xl">
+						<CategoryName category={movement.category} />
+						<span
+							className={cn(
+								movement.category.type === CategoryType.income && 'text-constructive',
+								movement.category.type === CategoryType.expense && 'text-destructive',
+							)}>
+							{convertCentsToCurrencyString(movement.amount)}
+						</span>
 					</div>
+					{/*	Bottom row */}
+					<div className="text-muted-foreground flex justify-between">
+						{/*	Origin and destination*/}
+						<div className="flex">
+							{movement.wallet.originId && (
+								<WalletIcon className="h-5 w-5 md:h-6 md:w-6" walletId={movement.wallet.originId} />
+							)}
+							{movement.wallet.originId && movement.wallet.destinationId && (
+								<ArrowBigRight className="h-5 w-5 md:h-6 md:w-6" />
+							)}
+							{movement.wallet.destinationId && (
+								<WalletIcon
+									className="h-5 w-5 md:h-6 md:w-6"
+									walletId={movement.wallet.destinationId}
+								/>
+							)}
+						</div>
 
-					{/*	Description */}
-					<span className="flex-1 pl-2">{capitalizeFirst(movement.description)}</span>
+						{/*	Description */}
+						<span className="flex-1 pl-2">{capitalizeFirst(movement.description)}</span>
 
-					{/* Date */}
-					<span>{convertIsoDate(movement.date)}</span>
+						{/* Date */}
+						<span>{convertIsoDate(movement.date)}</span>
+					</div>
 				</div>
+
+				{/* Menu, only visible in web view */}
+				{!isMobile && (
+					<DropDrawer>
+						{/*	Button */}
+						<DropDrawerTrigger asChild>
+							<Button variant="secondary" size="icon-sm" className="md:ml-3">
+								<EllipsisVertical />
+							</Button>
+						</DropDrawerTrigger>
+
+						{/*	Menu content */}
+						<DropDrawerContent align="start">
+							<DropDrawerGroup>
+								<DropDrawerItem onClick={() => setOpenEdit(true)} icon={<Pencil />}>
+									<span>Edit Movement</span>
+								</DropDrawerItem>
+								<DropDrawerItem
+									onClick={() => setOpenDelete(true)}
+									icon={<Trash />}
+									variant="destructive">
+									<span>Delete Movement</span>
+								</DropDrawerItem>
+							</DropDrawerGroup>
+						</DropDrawerContent>
+					</DropDrawer>
+				)}
 			</div>
 
-			{/* Menu */}
-			<DropDrawer>
-				{/*	Button */}
-				<DropDrawerTrigger asChild>
-					<Button variant="secondary" size="icon-sm" className="md:ml-3">
-						<EllipsisVertical />
-					</Button>
-				</DropDrawerTrigger>
-
-				{/*	Menu content */}
-				<DropDrawerContent align="start">
-					<DropDrawerGroup>
-						<DropDrawerItem onClick={() => setOpenEdit(true)} icon={<Pencil />}>
-							<span>Edit Movement</span>
-						</DropDrawerItem>
-						<DropDrawerItem
-							onClick={() => setOpenDelete(true)}
-							icon={<Trash />}
-							variant="destructive">
-							<span>Delete Movement</span>
-						</DropDrawerItem>
-					</DropDrawerGroup>
-				</DropDrawerContent>
-
-				{/* Forms	*/}
-				<EditMovementDialog id={movement.id} open={openEdit} setOpen={setOpenEdit} />
-				<DeleteMovementDialog id={movement.id} open={openDelete} setOpen={setOpenDelete} />
-			</DropDrawer>
-		</div>
+			{/* Dialog Forms	*/}
+			<EditMovementDialog id={movement.id} open={openEdit} setOpen={setOpenEdit} />
+			<DeleteMovementDialog id={movement.id} open={openDelete} setOpen={setOpenDelete} />
+		</>
 	)
 }
