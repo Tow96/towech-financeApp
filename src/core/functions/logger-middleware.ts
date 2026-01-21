199cdc74651def854993ab1c6c56a3a21695438e
tow96 <jose.towe@gmail.com>
2026-01-21 11:50:24 -0600

    Fixed log verbosity

--- src/core/functions/logger-middleware.ts
+++ src/core/functions/logger-middleware.ts
@@ -15,7 +15,10 @@ export const loggingMiddleware = createMiddleware().server(async ({ next }) => {
 	const method = result.request.method
 	const functionName = new URL(result.request.url).pathname
 	const status = result.response.status
-	logger.info({ method, functionName, status, message: `${method} ${functionName} - ${status}` })
+	if (functionName.startsWith('/_serverFn'))
+		logger.trace({ method, functionName, status, message: `${method} ${functionName} - ${status}` })
+	else
+		logger.info({ method, functionName, status, message: `${method} ${functionName} - ${status}` })
 
 	result.response.headers.set(CORRELATION_ID_HEADER, corrId)
 	return result
