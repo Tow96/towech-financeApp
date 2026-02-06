65d55136dc87de8efee82b6667eed9013a597eb2
Jose Tow <jose.towe@gmail.com>
2026-02-05 20:10:53 -0600

    Fixed timezone issue

--- src/core/functions/analysis-get-balance.ts
+++ src/core/functions/analysis-get-balance.ts
@@ -1,4 +1,4 @@
-import { eq, sql, sum } from 'drizzle-orm'
+import { and, eq, gte, lte, sql, sum } from 'drizzle-orm'
 import { createServerFn } from '@tanstack/react-start'
 
 import { AuthorizationMiddleware } from './session-validate'
@@ -9,89 +9,89 @@ import { db, schema } from '@/database'
 export const getBalanceAnalysis = createServerFn({ method: 'GET' })
 	.middleware([AuthorizationMiddleware])
 	.handler(async ({ context: { userId, logger } }) => {
-		try {
-			const from = new Date(Date.UTC(2026, 0, 4))
-			const to = new Date(Date.UTC(2026, 0, 19))
+		const from = new Date(Date.UTC(2026, 0, 1, 0, 0, 0, 0))
+		const to = new Date(Date.UTC(2026, 0, 8, 23, 59, 59, 999))
 
-			logger.info(
-				`User ${userId} requesting balance chart from: ${from.toISOString()} to: ${to.toISOString()}`,
-			)
+		logger.info(
+			`User ${userId} requesting balance chart from: ${from.toISOString().substring(0, 10)} to: ${to.toISOString().substring(0, 10)}`,
+		)
 
-			const dailyMovements = await db
-				.select({
-					date: schema.Movements.date,
-					income: sum(
-						sql`CASE WHEN ${schema.Movements.categoryType}=${CategoryType.income} THEN ${schema.MovementSummary.amount} ELSE 0 END`,
-					)
-						.mapWith(Number)
-						.as('income'),
-					expense: sum(
-						sql`CASE WHEN ${schema.Movements.categoryType}=${CategoryType.expense} THEN ${schema.MovementSummary.amount} ELSE 0 END`,
-					)
-						.mapWith(Number)
-						.as('expense'),
-					total: sum(
-						sql`CASE
+		const dailyMovements = await db
+			.select({
+				date: schema.Movements.date,
+				income: sum(
+					sql`CASE WHEN ${schema.Movements.categoryType}=${CategoryType.income} THEN ${schema.MovementSummary.amount} ELSE 0 END`,
+				)
+					.mapWith(Number)
+					.as('income'),
+				expense: sum(
+					sql`CASE WHEN ${schema.Movements.categoryType}=${CategoryType.expense} THEN ${schema.MovementSummary.amount} ELSE 0 END`,
+				)
+					.mapWith(Number)
+					.as('expense'),
+				total: sum(
+					sql`CASE
 						WHEN ${schema.Movements.categoryType}=${CategoryType.income} THEN ${schema.MovementSummary.amount}
 						WHEN ${schema.Movements.categoryType}=${CategoryType.expense} THEN -${schema.MovementSummary.amount}
 						ELSE 0
 					END`,
-					)
-						.mapWith(Number)
-						.as('total'),
-				})
-				.from(schema.Movements)
-				.leftJoin(
-					schema.MovementSummary,
-					eq(schema.Movements.id, schema.MovementSummary.movementId),
 				)
-				.where(eq(schema.Movements.userId, userId))
-				.groupBy(schema.Movements.date)
-				.orderBy(schema.Movements.date)
-				.as('daily_movements')
+					.mapWith(Number)
+					.as('total'),
+			})
+			.from(schema.Movements)
+			.leftJoin(schema.MovementSummary, eq(schema.Movements.id, schema.MovementSummary.movementId))
+			.where(eq(schema.Movements.userId, userId))
+			.groupBy(schema.Movements.date)
+			.orderBy(schema.Movements.date)
+			.as('daily_movements')
 
-			const targetDays = db
-				.select({ pate: sql<Date>`date`.mapWith(x => new Date(x)).as('pate') })
-				.from(
-					sql`(SELECT generate_series(${from}::date, ${to}::date, '1 day'::interval)::date AS date)`,
-				)
-				.as('target_days')
+		const targetDays = await db
+			.select({ pate: sql<Date>`date`.mapWith(x => new Date(x)).as('pate') })
+			.from(
+				sql.raw(
+					`(SELECT generate_series('${from.getUTCFullYear()}-${from.getUTCMonth() + 1}-${from.getUTCDate()}'::date, '${to.getUTCFullYear()}-${to.getUTCMonth() + 1}-${to.getUTCDate()}'::date, '1 day'::interval)::date AS date)`,
+				),
+			)
+			.as('target_days')
 
-			const highResDailyMovements = db
-				.select({
-					date: sql<Date>`COALESCE(${dailyMovements.date}, ${targetDays.pate})`.as('date'),
-					income: sql<number>`COALESCE(${dailyMovements.income}, 0)`.as('income'),
-					expense: sql<number>`COALESCE(${dailyMovements.expense},0)`.as('expense'),
-					total: sql<number>`COALESCE(${dailyMovements.total}, 0)`.as('total'),
-				})
-				.from(dailyMovements)
-				.fullJoin(targetDays, eq(dailyMovements.date, targetDays.pate))
-				.as('highResDailyMovements')
+		const highResDailyMovements = db
+			.select({
+				date: sql<Date>`COALESCE(${dailyMovements.date}, ${targetDays.pate})`
+					.mapWith(x => new Date(x))
+					.as('date'),
+				income: sql<number>`COALESCE(${dailyMovements.income}, 0)`.as('income'),
+				expense: sql<number>`COALESCE(${dailyMovements.expense},0)`.as('expense'),
+				total: sql<number>`COALESCE(${dailyMovements.total}, 0)`.as('total'),
+			})
+			.from(dailyMovements)
+			.fullJoin(targetDays, eq(dailyMovements.date, targetDays.pate))
+			.as('highResDailyMovements')
 
-			const dailyBalance = await db
-				.select({
-					date: highResDailyMovements.date,
-					income: sql<number>`SUM(${highResDailyMovements.income}) OVER(ORDER BY ${highResDailyMovements.date})`,
-					expense: sql<number>`SUM(${highResDailyMovements.expense}) OVER(ORDER BY ${highResDailyMovements.date})`,
-					total: sql<number>`SUM(${highResDailyMovements.total}) OVER(ORDER BY ${highResDailyMovements.date})`,
-				})
-				.from(highResDailyMovements)
-				.orderBy(highResDailyMovements.date)
+		const dailyBalance = db
+			.select({
+				date: highResDailyMovements.date,
+				income:
+					sql<number>`SUM(${highResDailyMovements.income}) OVER(ORDER BY ${highResDailyMovements.date})`.as(
+						'income',
+					),
+				expense:
+					sql<number>`SUM(${highResDailyMovements.expense}) OVER(ORDER BY ${highResDailyMovements.date})`.as(
+						'expense',
+					),
+				total:
+					sql<number>`SUM(${highResDailyMovements.total}) OVER(ORDER BY ${highResDailyMovements.date})`.as(
+						'total',
+					),
+			})
+			.from(highResDailyMovements)
+			.orderBy(highResDailyMovements.date)
+			.as('dailyBalance')
 
-			logger.debug(dailyBalance)
+		const result = await db
+			.select()
+			.from(dailyBalance)
+			.where(and(gte(dailyBalance.date, from), lte(dailyBalance.date, to)))
 
-			return [
-				{ date: new Date('2026-01-18'), balance: 186 },
-				{ date: new Date('2026-01-19'), balance: 305 },
-				{ date: new Date('2026-01-20'), balance: 237 },
-				// { date: new Date('2026-01-21'), balance: 73 },
-				{ date: new Date('2026-01-22'), balance: 209 },
-				{ date: new Date('2026-01-23'), balance: 214 },
-				{ date: new Date('2026-01-24'), balance: 214 },
-			]
-		} catch (e) {
-			logger.error(e)
-			return e
-		}
+		return result.map(x => ({ date: new Date(x.date), balance: x.total }))
 	})
-
